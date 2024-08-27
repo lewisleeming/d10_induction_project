54d867e4d7816eb5efd7de412343efae5b63d063
Lewis Leeming <lewisleeming@Admins-MBP.localdomain>
2024-08-27 15:24:44 +0100

    LEW-64: services unfinished

--- web/modules/custom/vehicle_custom/src/Service/VehicleLookupService.php
+++ web/modules/custom/vehicle_custom/src/Service/VehicleLookupService.php
@@ -2,18 +2,49 @@
 
 declare(strict_types=1);
 
-namespace Drupal\vehicle_custom;
+namespace Drupal\vehicle_custom\Service;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * @todo Add class description.
  */
-final class VehicleLookup {
+final class VehicleLookupService {
+
+  /**
+   * @var \Psr\Log\LoggerInterface
+   */
+  private LoggerInterface $logger;
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private EntityTypeManagerInterface $entityTypeManager;
 
   /**
    * @todo Add method description.
    */
-  public function doSomething(): void {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, LoggerInterface $logger) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->logger = $logger;
+  }
+  public function getVehicleTitleByNid($node_id){
     // @todo Place your code here.
+    $node = $this->entityTypeManager->getStorage('node')->load($node_id);
+    $title = $node->label();
+
+    \Drupal::logger('vehicle_custom')->info('The following nid: @nodeid was requested, and the following output returned: @node_title',[
+      '@nodeid' => $node_id,
+      '@node_title' => $title,
+    ]);
+    //doesnt work
+//    $this->logger->warning('The following nid: @nodeid was requested, and the following output returned: @node_title',[
+//      '@nodeid' => $node_id,
+//      '@node_title' => $title,
+//    ]);
+
+    return $title;
+
   }
 
 }
